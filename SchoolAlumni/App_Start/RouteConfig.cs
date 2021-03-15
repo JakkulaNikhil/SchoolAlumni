7a9f9d1734908a62213d900babffbd4da8bff395
Nikhil <jakkulanikhil99@gmail.com>
2021-03-15 23:44:18 +0530

    error

--- SchoolAlumni/App_Start/RouteConfig.cs
+++ SchoolAlumni/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SchoolAlumni
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "School", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
