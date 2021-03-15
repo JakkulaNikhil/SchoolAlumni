c31c623e2222cc713072d15ad04fec4913d0cb7b
Nikhil <jakkulanikhil99@gmail.com>
2021-03-15 23:25:43 +0530

    ui

--- SchoolAlumni/App_Start/BundleConfig.cs
+++ SchoolAlumni/App_Start/BundleConfig.cs
@@ -9,22 +9,25 @@ namespace SchoolAlumni
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/assets/vendor/jquery/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Content/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                      "~/Content/assets/vendor/jquery.easing/jquery.easing.min.js",
+                      "~/Content/assets/vendor/waypoints/jquery.waypoints.min.js",
+                      "~/Content/assets/vendor/counterup/counterup.min.js",
+                      "~/Content/assets/vendor/owl.carousel/owl.carousel.min.js",
+                      "~/Content/assets/vendor/isotope-layout/isotope.pkgd.min.js",
+                      "~/Content/assets/vendor/venobox/venobox.min.js",
+                      "~/Content/assets/js/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/assets/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/assets/vendor/icofont/icofont.min.css",
+                      "~/Content/assets/vendor/boxicons/css/boxicons.min.css",
+                      "~/Content/assets/vendor/owl.carousel/assets/owl.carousel.min.css",
+                      "~/Content/assets/vendor/venobox/venobox.css",
+                      "~/Content/assets/css/style.css"));
         }
     }
 }
